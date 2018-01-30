623deacdfadfb4b96f9196630dd5a64fe3378a70
jingApril <kady762@163.com>
2018-01-30 17:10:56 +0800

    know how to use StdRandom

--- Percolation.java
+++ Percolation.java
@@ -10,19 +10,27 @@ public class Percolation{
       private final int OPENED = 0;
       private final int FULL_OPENED = 1;
       
-      public Percolation(int n) //create n-by-n grind, with all sites clocked
-      {
-          this.n = n;
-          grid = new int[n][n];
-          for(int row = 0;  row < n; row ++) {
-              for(int col = 0; col <n; col ++) {
-               grid[row][col] = BLCOKED;
-              }
-          }       
-         unionInstance  = new  WeightedQuickUnionUF(n^2 + 2);
+      public Percolation(int n)  { //create n-by-n grind, with all sites clocked
+    
+             this.n = n;
+             grid = new int[n][n];
+             for(int row = 0;  row < n; row ++) {
+                   for(int col = 0; col <n; col ++) {
+                         grid[row][col] = BLCOKED;
+                   }
+             }       
+             unionInstance  = new  WeightedQuickUnionUF(n^2 + 2);
       }
       
+      public  void open(int row, int col) { //open site(row, col) if it is not open already
+          
+             row = StdRandom.uniform( n-1);
+             col = StdRandom.uniform(n-1);
+            while ( grid[row][col]==BLCOKED) {
+                grid[row][col] = OPENED;
+            }
       
+      }
       public boolean isOpen(int row, int col) //open site(row, col) if it is not open already    
       {      
               if(row <=0 || row > n || col < 0 || col > n)   throw new IllegalException(" Invalid arguments");
@@ -32,15 +40,14 @@ public class Percolation{
       public boolean isFull(int row, int col) // is site(row, col) open?
       {
             if(row <=0 || row > n || col < 0 || col > n)   throw new IllegalException(" Invalid arguments");
-            isValidBounds(row,col);
             return (grid[row][col] == FULLY_OPENED ? true : false );    
       }
       
       public int numberOfOpenSites() {
           int opened = 0;
-          for( int row = 0; row < this.n; row ++) {
-              for(int col = 0; col < this.n; col +++) {
-                  if(isOpen(row, col) {
+          for( int row = 0;  row < n;  row++) {
+              for(int col = 0;  col < n;  col++) {
+                   if(isOpen(row, col)){
                       opened ++;
                   }
                }
@@ -49,43 +56,14 @@ public class Percolation{
       }
       
     public boolean percolates() {
-       return unionInstance.connected(1, n);
+       return unionInstance.connected(0, n* n+1);
     }
     
     private int xyTo1D(int row, int col) {
-           return row*n + col+1;
+           return row*n + col;
     }
       
-      private int root(int i) {
-          while( i != parents[i]) {
-              parents[i] = parents[parents[i]];
-              i = parents[i];
-          }
-          return i;
-      }
-      
-      private void union(int i, int j) {
-          int rooti = root(i);
-          int rooj = root(j);
-          if(size[rooti] > size[rootj]) {
-             parents[rootj] = rooti;
-             size[rooti] += size[rootj];
-          } else {
-                parents[rooti] = rootj;
-                size[rootj] += size[rooti];
-          }
-      }
-      
-      private boolean isConnected( int i, int j) {
-        return ( root(i) == root(j));
-      }
-      
-      public boolean percolates( ) // does the system percolste?
-      {
-          return isConnected( 0, n* n+1);
-      }
-      
-      public static void main(String[] args) {
+    public static void main(String[] args) {
       
       
       }
